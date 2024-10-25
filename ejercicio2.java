068d7f01e8723c3a2140399d64145e0cbe0dc0c9
Adrian-Lorca <alormor0107b@g.educaand.es>
2024-10-25 19:00:24 +0200

    Add files via upload

---
+++ ejercicio2.java
@@ -0,0 +1,73 @@
+public class ejercicio2 {
+    public static void main(String[] args) {
+    //a
+      int edad = 20;
+      System.out.println(edad);
+    //b
+        int codigopostal = 18230;
+        System.out.println(codigopostal);
+    //c
+        double altura = 1.85;
+        System.out.println(altura);
+    //d
+        char hombre = 'H';
+        char mujer = 'M';
+        System.out.println(hombre);
+        System.out.println(mujer);
+    //e
+        String nombre = "Adrian";
+        System.out.println(nombre);
+    //f
+        int numerodehijos = 5;
+        System.out.println(numerodehijos);
+    //g
+        double iva = 21.59;
+        System.out.println(iva);
+    //h
+        int camisa = 40;
+        System.out.println(camisa);
+    //i
+        double peso = 72.5;
+        System.out.println(peso);
+    //j
+        double precio = 59.99;
+        System.out.println(precio);
+    //k
+        boolean alumno = true;
+        boolean alumnoo = false;
+
+        System.out.println(alumno);
+        System.out.println(alumnoo);
+    //l
+        String mensaje = "Fran puto calvo";
+        System.out.println(mensaje);
+    //m
+        char letra = 'A';
+        System.out.println(letra);
+    //n
+        boolean mayoredad = true;
+        boolean mayoredad2 = false;
+        System.out.println(mayoredad);
+        System.out.println(mayoredad2);
+    //o
+        int minutos = 60;
+        System.out.println(minutos);
+    //p
+        int dias = 7;
+        System.out.println(dias);
+    //q
+        String matricula = "56784AFG";
+        System.out.println(matricula);
+    //r
+        int contador = 1;
+        System.out.println(contador);
+    //s
+        boolean mayordeedad1 = false;
+        boolean mayordeedad2 = true;
+    //t
+        char talla1 = 'L';
+        char talla2 = 'M';
+        System.out.println(talla1);
+        System.out.println(talla2);
+    }
+}
