9941c80cb1ff56484e7ece965d78eeccbc1d4af0
Adrian-Lorca <alormor0107b@g.educaand.es>
2024-10-25 18:09:06 +0200

    Add files via upload

---
+++ ejercicio1.java
@@ -0,0 +1,85 @@
+public class ejercicio1 {
+    public static void main(String[] args) {
+       //a
+        int q = 5;
+        
+        int p = 6;
+
+        System.out.println(p);
+        System.out.println(q);
+        //b
+        float letra = 'x';
+        float letrita = 'y';
+        float letraza = 'z';
+        
+       System.out.println(letra);
+       System.out.println(letrita);
+       System.out.println(letraza);
+        //c
+       char variable1 = 'a';
+       char variable2 = 'b';
+       char variable3 = 'c';
+
+       System.out.println(variable1);
+       System.out.println(variable2);
+       System.out.println(variable3);
+        //d
+       Double raiz1 = 5.0;
+       Double raiz2 = 9.0;
+
+       System.out.println(raiz1);
+       System.out.println(raiz2);
+        //e
+       long contador = 0;
+
+       contador = 123456789L + 1;
+
+       System.out.println(contador);
+        //f
+        short indicador = 0;
+
+        indicador = 18;
+
+        System.out.println(indicador);
+        //g
+        int indice = 9;
+        System.out.println(indice);
+
+
+        //h
+        double precio;
+        precio = 78;
+        double preciofinal;
+        preciofinal = 70;
+
+        System.out.println(precio);
+        System.out.println(preciofinal);
+        //i
+        char car1 = 'T';
+        char car2 = 'U';
+
+        System.out.println(car1);
+        System.out.println(car2);
+
+        //j
+        byte valor = 0;
+
+        valor = 6;
+
+        System.out.println(valor);
+
+        //k
+        boolean primero = true;
+        boolean ultimo = false;
+
+     
+        System.out.println("Eres el primero: " + primero);
+        System.out.println("Eres el ultimo: " + ultimo);
+        //l
+        String nombre = "Adrian";
+        System.out.println(nombre);
+    }
+        
+        
+        
+}
