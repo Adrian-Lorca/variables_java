2767fe2d139392ab828db39734185446f649467c
Adrian-Lorca <alormor0107b@g.educaand.es>
2024-11-04 18:35:50 +0100

    Add files via upload

---
+++ ejercicio3.java
@@ -0,0 +1,120 @@
+public class ejercicio3 {
+    public static void main(String[] args) {
+        //a
+        float variable = 3/2;
+        float variable2 = 4/3;
+
+       float variables = variable + variable2;
+
+       System.out.println(variables);
+
+       //b 
+        float x = 2.0f;
+        float y = 3.0f;
+
+        float numerador = (1.0f) - (3.0f * x * y);
+        float denominador = (x - 5.0f) - (4.0f);
+
+        float resta = numerador - denominador;
+        System.out.println(resta);
+         
+       //c
+
+       float num = (1.0f + 7.0f);
+       float den = (2.0f + 1.0f);
+
+       float sumaa = num + den;
+
+       System.out.println(sumaa);
+
+       //d
+
+       float nume = (7.0f + 1.0f);
+       float deno = (1.0f + 2.0f);
+
+       float resul = nume + deno;
+
+       System.out.println(resul);
+
+
+    //e
+
+       float a = 1.0f;
+       float b = 2.0f;
+       float c = 3.0f;
+       float d = 4.0f;
+       float e = 5.0f;
+       float f = 6.0f;
+       float g = 7.0f;
+       float h = 8.0f;
+       float j = 9.0f;
+
+       float nuum = (a * a + d - e);
+       float deen = ( b - c + f - g * h/j);
+       
+       float solucion = nuum + deen;
+
+       System.out.println(solucion);
+
+       //f 
+
+       float m = 1.0f;
+       float n = 1.5f;
+       float p = 2.0f;
+
+       float numer = (m + p);
+       float denom = (n + 1.0f);
+
+       float respuesta = numer + denom;
+
+       System.out.println(respuesta);
+
+       //g
+
+       float q = 1.5f;
+
+       float numerador1 = (m + n);
+       float denominador1 = (1.0f + p - q);
+       
+       float suma_variables = numerador1 + denominador1;
+
+       System.out.println(suma_variables);
+
+
+       //h
+
+       float numera = (a * a + c * c);
+       float denomi = (b * b + d * d);
+
+       float resultados = numera + denomi;
+
+       System.out.println(resultados);
+
+       //i
+
+       float r = 1.5f;
+       float s = 2.0f;
+
+       float division = (m + n/p);
+       float division2 = (q - r/s);
+
+       float resultado_division = division / division2;
+
+       System.out.println(resultado_division);
+
+       //j
+
+       float division3 = (3.0f * a + b);
+       float division4 = (c - d + 5.0f * e / f + g / 2.0f * h);
+       float resultado_div = division3 / division4;
+       System.out.println(resultado_div);
+
+       //k
+
+       float division5 = (a * a + 2 * a * b + b * b);
+       float division6 = (1/x * x + 2);
+       float resultado_div2 = division5 / division6;
+       System.out.println(resultado_div2);
+    }
+
+}
---
+++ ejercicio4.java
@@ -0,0 +1,67 @@
+public class ejercicio4 {
+    public static void main(String[] args) {
+        // definiendo variables
+                int X = 10; 
+                int Y = 5;  
+                int N = 3;  
+                int M = 2;  
+        
+                // a. El valor de cada variable
+                System.out.println("X = " + X);
+                System.out.println("Y = " + Y);
+                System.out.println("N = " + N);
+                System.out.println("M = " + M);
+        
+                // b. La suma X + Y
+                System.out.println("Suma X + Y = " + (X + Y));
+        
+                // c. La diferencia X - Y
+                System.out.println("Diferencia X - Y = " + (X - Y));
+        
+                // d. El producto X * Y
+                System.out.println("Producto X * Y = " + (X * Y));
+        
+                // e. El cociente X / Y
+                System.out.println("Cociente X / Y = " + (X / Y));
+        
+                // f. El resto X % Y
+                System.out.println("Resto X % Y = " + (X % Y));
+        
+                // g. La suma N + M
+                System.out.println("Suma N + M = " + (N + M));
+        
+                // h. La diferencia N - M
+                System.out.println("Diferencia N - M = " + (N - M));
+        
+                // i. El producto N * M
+                System.out.println("Producto N * M = " + (N * M));
+        
+                // j. El cociente N / M
+                System.out.println("Cociente N / M = " + (N / M));
+        
+                // k. El resto N % M
+                System.out.println("Resto N % M = " + (N % M));
+        
+                // l. La suma X + N
+                System.out.println("Suma X + N = " + (X + N));
+        
+                // m. El cociente Y / M
+                System.out.println("Cociente Y / M = " + (Y / M));
+        
+                // n. El resto Y % M
+                System.out.println("Resto Y % M = " + (Y % M));
+        
+                // o. El doble de cada variable
+                System.out.println("Doble de X = " + (2 * X));
+                System.out.println("Doble de Y = " + (2 * Y));
+                System.out.println("Doble de N = " + (2 * N));
+                System.out.println("Doble de M = " + (2 * M));
+        
+                // p. La suma de todas las variables
+                System.out.println("Suma de todas las variables = " + (X + Y + N + M));
+        
+                // q. El producto de todas las variables
+                System.out.println("Producto de todas las variables = " + (X * Y * N * M));
+            }
+       }
+        
